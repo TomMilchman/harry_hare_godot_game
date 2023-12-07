9c2ef05fbbe5232eac04dc3b379eb399902b2f77
Tom Milchman <tom.milchman@post.runi.ac.il>
2023-12-07 16:09:40 +0200

    V0.0.20 Added more rooms (up to room 9)

--- scenes/objects/checkpoint_flag.gd
+++ scenes/objects/checkpoint_flag.gd
@@ -4,7 +4,6 @@ var room_location: String
 
 func _ready():
 	room_location = get_tree().current_scene.scene_file_path
-	print(room_location)
 	
 	if (PlayerGlobals.spawn_room_location != room_location or PlayerGlobals.spawn_pos != global_position):
 		$AnimationPlayer.play("lowered")
